bdc6d6c084843d9bea9e12baea1fe8f7ea1351b4
ElhanouniYassine <149019283+ElhanouniYassine@users.noreply.github.com>
2025-10-24 20:52:21 +0100

    Delete src directory

--- src/main/java/com/ensias/springdatarest/modele/ProprietaireRepo.java
+++
@@ -1,6 +0,0 @@
-package com.ensias.springdatarest.modele;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProprietaireRepo extends CrudRepository<Proprietaire, Long> {
-}
